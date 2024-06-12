67fedd385df7bbab2ac7e389ba32b872846d0ece
Giovane Lucas <gio1player@gmail.com>
2024-06-12 10:34:14 -0300

    Criado o programa principal

---
+++ src/Aplicativo/Programa.java
@@ -0,0 +1,65 @@
+package Aplicativo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entidades.Contribuinte;
+import Entidades.PessoaFisica;
+import Entidades.PessoaJuridica;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.print("Digite o numero de contribuintes: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i<n ; i++) {
+			
+			System.out.println("Contribuinte #"+(i+1)+ "dados:");
+			System.out.print("Pessoa Fisica ou juridica (f/j)?");
+			sc.nextLine();
+			char escolha = sc.nextLine().charAt(0);
+			
+			List<Contribuinte> list = new ArrayList<>(); 
+			
+			System.out.print("Nome: ");
+			String nome = sc.nextLine();
+			System.out.print("Renda Anual: ");
+			Double rendaAnual = sc.nextDouble();
+			
+			if (escolha == 'f'){
+					System.out.print("Gastos com saúde: ");
+					Double gastosSaude = sc.nextDouble();
+					
+				PessoaFisica pf = new PessoaFisica(nome , rendaAnual , gastosSaude);
+				list.add(pf);
+			}else {
+				System.out.println("Numero de funcionarios: ");
+				Integer numeroDeFuncionarios = sc.nextInt();
+				
+				PessoaJuridica pj = new PessoaJuridica(nome , rendaAnual, numeroDeFuncionarios );
+				list.add(pj);
+			}
+			
+			
+			System.out.println("Impostos dos contribuintes:" );
+			
+			for (Contribuinte c : list) {
+				System.out.println(c.getName() + "$ "+String.format("%.2f", c.ImpostoPago()));
+				
+			}
+			
+			sc.close();
+			
+		}
+		
+	
+	}
+
+}
